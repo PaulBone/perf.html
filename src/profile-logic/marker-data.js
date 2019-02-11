firefox-devtools/profiler

Add selectors for GC marker types
This change adds three selectors for preview-filtered lists of GC marker
events.  It also adds the required types.

src/profile-logic/marker-data.js
@@ -8,7 +8,12 @@ import type {
   MarkersTable,
   IndexIntoStringTable,
 } from '../types/profile';
-import type { TracingMarker } from '../types/profile-derived';
+import type {
+  TracingMarker,
+  GCMinorMarker,
+  GCSliceMarker,
+  GCMajorMarker,
+} from '../types/profile-derived';
 import type { BailoutPayload, ScreenshotPayload } from '../types/markers';
 import type { StartEndRange } from '../types/units';
 import type { UniqueStringArray } from '../utils/unique-string-array';
@@ -350,6 +355,16 @@ export function isNetworkMarker(marker: TracingMarker): boolean {
   return !!(marker.data && marker.data.type === 'Network');
 }
 
+function isGCMinorMarker(marker: TracingMarker): boolean {
+  return !!marker.data && marker.data.type === 'GCMinor';
+}
+function isGCSliceMarker(marker: TracingMarker): boolean {
+  return !!marker.data && marker.data.type === 'GCSlice';
+}
+function isGCMajorMarker(marker: TracingMarker): boolean {
+  return !!marker.data && marker.data.type === 'GCMajor';
+}
+
 export function filterForNetworkChart(markers: TracingMarker[]) {
   return markers.filter(marker => isNetworkMarker(marker));
 }
@@ -357,6 +372,17 @@ export function filterForNetworkChart(markers: TracingMarker[]) {
 export function filterForMarkerChart(markers: TracingMarker[]) {
   return markers.filter(marker => !isNetworkMarker(marker));
 }
+
+export function filterGCMinor(markers: TracingMarker[]): GCMinorMarker[] {
+  return (markers.filter(marker => isGCMinorMarker(marker)): any);
+}
+export function filterGCSlice(markers: TracingMarker[]): GCSliceMarker[] {
+  return (markers.filter(marker => isGCSliceMarker(marker)): any);
+}
+export function filterGCMajor(markers: TracingMarker[]): GCMajorMarker[] {
+  return (markers.filter(marker => isGCMajorMarker(marker)): any);
+}
+
 // Firefox emits separate start and end markers for each load. It does this so that,
 // if a profile is collected while a request is in progress, the profile will still contain
 // a start marker for that request. So by looking at start markers we can get

src/types/profile-derived.js
@@ -4,7 +4,12 @@
 
 // @flow
 import type { Milliseconds } from './units';
-import type { MarkerPayload } from './markers';
+import type {
+  MarkerPayload,
+  GCMinorMarkerPayload,
+  GCSliceMarkerPayload,
+  GCMajorMarkerPayload,
+} from './markers';
 import type { IndexIntoFuncTable, ThreadIndex, Pid } from './profile';
 export type IndexIntoCallNodeTable = number;
 
@@ -65,13 +70,19 @@ export type CallNodeInfo = {
  */
 export type CallNodePath = IndexIntoFuncTable[];
 
-export type TracingMarker = {
+export type Marker<T> = {
   start: Milliseconds,
   dur: Milliseconds,
   name: string,
   title: string | null,
-  data: MarkerPayload,
+  data: T,
 };
+
+export type TracingMarker = Marker<MarkerPayload>;
+
+export type GCMinorMarker = Marker<GCMinorMarkerPayload>;
+export type GCSliceMarker = Marker<GCSliceMarkerPayload>;
+export type GCMajorMarker = Marker<GCMajorMarkerPayload>;
 
 export type IndexIntoTracingMarkers = number;
 

PaulBone/perf.html

src/profile-logic/marker-data.js
@@ -8,8 +8,14 @@ import type {
   RawMarkerTable,
   IndexIntoStringTable,
 } from '../types/profile';
-import type { Marker } from '../types/profile-derived';
-import type { BailoutPayload } from '../types/markers';
+import type {
+  Marker,
+  GCMinorMarker,
+  GCSliceMarker,
+  GCMajorMarker,
+} from '../types/profile-derived';
+import type { BailoutPayload, ScreenshotPayload } from '../types/markers';
+import type { StartEndRange } from '../types/units';
 import type { UniqueStringArray } from '../utils/unique-string-array';
 import { getNumberPropertyOrNull } from '../utils/flow';
 
@@ -413,6 +419,16 @@ export function isNavigationMarker({ name, data }: Marker) {
   return false;
 }
 
+function isGCMinorMarker(marker: Marker): boolean {
+  return !!marker.data && marker.data.type === 'GCMinor';
+}
+function isGCSliceMarker(marker: Marker): boolean {
+  return !!marker.data && marker.data.type === 'GCSlice';
+}
+function isGCMajorMarker(marker: Marker): boolean {
+  return !!marker.data && marker.data.type === 'GCMajor';
+}
+
 export function filterForNetworkChart(markers: Marker[]) {
   return markers.filter(marker => isNetworkMarker(marker));
 }
@@ -420,6 +436,17 @@ export function filterForNetworkChart(markers: Marker[]) {
 export function filterForMarkerChart(markers: Marker[]) {
   return markers.filter(marker => !isNetworkMarker(marker));
 }
+
+export function filterGCMinor(markers: Marker[]): GCMinorMarker[] {
+  return (markers.filter(marker => isGCMinorMarker(marker)): any);
+}
+export function filterGCSlice(markers: Marker[]): GCSliceMarker[] {
+  return (markers.filter(marker => isGCSliceMarker(marker)): any);
+}
+export function filterGCMajor(markers: Marker[]): GCMajorMarker[] {
+  return (markers.filter(marker => isGCMajorMarker(marker)): any);
+}
+
 // Firefox emits separate start and end markers for each load. It does this so that,
 // if a profile is collected while a request is in progress, the profile will still contain
 // a start marker for that request. So by looking at start markers we can get

src/types/profile-derived.js
@@ -4,7 +4,12 @@
 
 // @flow
 import type { Milliseconds } from './units';
-import type { MarkerPayload } from './markers';
+import type {
+  MarkerPayload,
+  GCMinorMarkerPayload,
+  GCSliceMarkerPayload,
+  GCMajorMarkerPayload,
+} from './markers';
 import type {
   IndexIntoFuncTable,
   ThreadIndex,
@@ -71,16 +76,24 @@ export type CallNodeInfo = {
  */
 export type CallNodePath = IndexIntoFuncTable[];
 
-export type Marker = {|
+export type MarkerT<T> = {|
   start: Milliseconds,
   dur: Milliseconds,
   name: string,
   title: string | null,
-  data: MarkerPayload,
+  data: T,
   incomplete?: boolean,
 |};
 
+export type Marker = MarkerT<MarkerPayload>;
+
+export type GCMinorMarker = MarkerT<GCMinorMarkerPayload>;
+export type GCSliceMarker = MarkerT<GCSliceMarkerPayload>;
+export type GCMajorMarker = MarkerT<GCMajorMarkerPayload>;
+
 export type IndexIntoMarkers = number;
+
+export type IndexIntoTracingMarkers = number;
 
 export type CallNodeData = {
   funcName: string,