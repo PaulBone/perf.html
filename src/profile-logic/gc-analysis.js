firefox-devtools/profiler

Calculate and display statistics about GC pauses
This computes things like the average, median, 90th percentile, and maximum
GC pause times (for different GC pause types).  It also computes total time
spent in the GC as a fraction of total execution time.

src/profile-logic/gc-analysis.js
@@ -7,8 +7,40 @@ import type {
   GCMinorMarker,
   GCSliceMarker,
   GCMajorMarker,
+  Marker,
   GCStats,
+  PauseInfo,
 } from '../types/profile-derived';
+import type { Milliseconds } from '../types/units';
+import * as stats from 'simple-statistics';
+
+function pauseInfo(durations: Milliseconds[]): PauseInfo | null {
+  if (durations.length === 0) {
+    return null;
+  }
+
+  durations.sort((a, b) => a - b);
+
+  const total = durations.reduce((a, b) => a + b, 0);
+  let max = stats.maxSorted(durations);
+  if (!max) {
+    max = 0;
+  }
+
+  return {
+    numberOfPauses: durations.length,
+    meanPause: total / durations.length,
+    stdDev: stats.standardDeviation(durations),
+    medianPause: stats.medianSorted(durations),
+    p90Pause: stats.quantileSorted(durations, 0.9),
+    maxPause: max,
+    totalPaused: total,
+  };
+}
+
+function _toDurations<T>(markers: Marker<T>[]): Milliseconds[] {
+  return markers.map(m => m.dur);
+}
 
 export function computeGCStats(
   minorMarkers: GCMinorMarker[],
@@ -16,8 +48,11 @@ export function computeGCStats(
   majorMarkers: GCMajorMarker[]
 ): GCStats {
   return {
-    numMinor: minorMarkers.length,
-    numSlice: sliceMarkers.length,
+    minorPauses: pauseInfo(_toDurations(minorMarkers)),
+    slicePauses: pauseInfo(_toDurations(sliceMarkers)),
+    allPauses: pauseInfo(
+      _toDurations(minorMarkers).concat(_toDurations(sliceMarkers))
+    ),
     numMajor: majorMarkers.length,
   };
 }

PaulBone/perf.html

src/profile-logic/gc-analysis.js
@@ -7,8 +7,40 @@ import type {
   GCMinorMarker,
   GCSliceMarker,
   GCMajorMarker,
+  MarkerT,
   GCStats,
+  PauseInfo,
 } from '../types/profile-derived';
+import type { Milliseconds } from '../types/units';
+import * as stats from 'simple-statistics';
+
+function pauseInfo(durations: Milliseconds[]): PauseInfo | null {
+  if (durations.length === 0) {
+    return null;
+  }
+
+  durations.sort((a, b) => a - b);
+
+  const total = durations.reduce((a, b) => a + b, 0);
+  let max = stats.maxSorted(durations);
+  if (!max) {
+    max = 0;
+  }
+
+  return {
+    numberOfPauses: durations.length,
+    meanPause: total / durations.length,
+    stdDev: stats.standardDeviation(durations),
+    medianPause: stats.medianSorted(durations),
+    p90Pause: stats.quantileSorted(durations, 0.9),
+    maxPause: max,
+    totalPaused: total,
+  };
+}
+
+function _toDurations<T>(markers: MarkerT<T>[]): Milliseconds[] {
+  return markers.map(m => m.dur);
+}
 
 export function computeGCStats(
   minorMarkers: GCMinorMarker[],
@@ -16,8 +48,11 @@ export function computeGCStats(
   majorMarkers: GCMajorMarker[]
 ): GCStats {
   return {
-    numMinor: minorMarkers.length,
-    numSlice: sliceMarkers.length,
+    minorPauses: pauseInfo(_toDurations(minorMarkers)),
+    slicePauses: pauseInfo(_toDurations(sliceMarkers)),
+    allPauses: pauseInfo(
+      _toDurations(minorMarkers).concat(_toDurations(sliceMarkers))
+    ),
     numMajor: majorMarkers.length,
   };
 }